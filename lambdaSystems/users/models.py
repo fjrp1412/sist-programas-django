d3a6f28cd59e90a6422d87d82b25d47541f927c6
Frnacisco <fruiz.rc@gmail.com>
2021-03-27 00:03:53 -0400

    add some fields and create classes for Profile, Admin and Salesman

--- lambdaSystems/users/models.py
+++ lambdaSystems/users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    """Base class for User and Admin classes."""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    identification_card = models.CharField(max_length=10)
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"User = {self.user.username}, Cedula= {self.identification_card}"
+
+
+class Admin(Profile):
+    """Model for user Admin, is a proxy class
+    because this class not require any extra data."""
+
+    class Meta:
+        proxy = True
+
+
+class Salesman(Profile):
+    """Model for seller model, this class is a 1to1 field
+    cause require extra info"""
+
+    seller = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    count_sells = models.IntegerField()
+    earnings = models.DecimalField()
