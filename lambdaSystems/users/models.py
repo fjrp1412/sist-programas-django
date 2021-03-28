c0a92e59343500f6aa0331a5bd3e3d15164ca81a
Frnacisco <fruiz.rc@gmail.com>
2021-03-28 19:43:48 -0400

    nothing work in this shit

--- lambdaSystems/users/models.py
+++ lambdaSystems/users/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     """Model base for Admin and Salesman."""
     name = models.CharField(max_length=120)
-    identifacion_document = models.CharField(max_length=10)
+    identification_document = models.CharField(max_length=10)
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
@@ -15,9 +15,11 @@ class Profile(models.Model):
 class Admin(Profile):
     """Model for admin user."""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    admin_id = models.AutoField(primary_key=True, unique=True, default=0)
 
 
 class Salesman(Profile):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     count_sells = models.IntegerField()
     earnings = models.DecimalField(max_digits=10, decimal_places=2)
+    seller_id = models.AutoField(primary_key=True, unique=True, default=0)
